97d4355787581ec49e9cbeffa36e92d4f4adb7e1
nwilson19 <51173865+nwilson19@users.noreply.github.com>
2019-08-17 12:04:46 -0500

    Trying to override the Equals extension method for my Contact class. It appears I cannot do this override. Should I just rename this to something similar and hope it isn't too confusing?

--- Contact/Extensions.cs
+++ Contact/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Nate.ContactApp
@@ -15,25 +16,12 @@ namespace Nate.ContactApp
             return properNumber;
         }
 
-        public static bool Equals(this Contact contact, Contact contact2)
+        public bool Equals(this Contact contact, Contact contact2)
         {
-            bool isEqual = true;
-            isEqual = contact.isActiveRecord == contact2.isActiveRecord;
-            if (isEqual)
-                isEqual = contact.contactID == contact2.contactID;
-            if (isEqual)
-                isEqual = contact.email == contact2.email;
-            if (isEqual)
-                isEqual = contact.firstName == contact2.firstName;
-            if (isEqual)
-                isEqual = contact.lastName == contact2.lastName;
-            if (isEqual)
-                isEqual = contact.CellPhone == contact2.CellPhone;
-            if (isEqual)
-                isEqual = contact.HomePhone == contact2.HomePhone;
-            if (isEqual)
-                isEqual = contact.WorkPhone == contact2.WorkPhone;
-            return isEqual;
+             return (contact.isActiveRecord == contact2.isActiveRecord && contact.contactID == contact2.contactID &&
+                     contact.email == contact2.email && contact.firstName == contact2.firstName &&
+                     contact.lastName == contact2.lastName && contact.CellPhone == contact2.CellPhone &&
+                     contact.HomePhone == contact2.HomePhone && contact.WorkPhone == contact2.WorkPhone);
         }
 
     }
