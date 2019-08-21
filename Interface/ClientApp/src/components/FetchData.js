d24eec1f08950e77fece105103719f36adafbbae
nwilson19 <51173865+nwilson19@users.noreply.github.com>
2019-08-20 21:14:14 -0500

    Moved the API into the React project I added. I have a partially working display of some contact data now, but need to run down why not all fields are being returned from the Get call as I would expect them to.

---
+++ Interface/ClientApp/src/components/FetchData.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+export class FetchData extends Component {
+  static displayName = FetchData.name;
+
+  constructor (props) {
+    super(props);
+    this.state = { contacts: [], loading: true };
+
+    fetch('api/contact')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ contacts: data, loading: false });
+      });
+  }
+
+  static renderContactsTable (contacts) {
+    return (
+      <table className='table table-striped'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Active Record</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Home Phone</th>
+            <th>Work Phone</th>
+            <th>Cell Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map(contact =>
+            <tr key={contact.contactID}>
+              <td>{contact.contactID}</td>
+              <td>{contact.isActiveRecord}</td>
+              <td>{contact.firstName}</td>
+              <td>{contact.lastName}</td>
+              <td>{contact.email}</td>
+              <td>{contact.phoneHome}</td>
+              <td>{contact.phoneWork}</td>
+              <td>{contact.phoneCell}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    );
+  }
+
+  render () {
+    let contents = this.state.loading
+      ? <p><em>Loading...</em></p>
+      : FetchData.renderContactsTable(this.state.contacts);
+
+    return (
+      <div>
+        <h1>Contact Get All</h1>
+        <p>This component demonstrates fetching data from the server.</p>
+        {contents}
+      </div>
+    );
+  }
+}
